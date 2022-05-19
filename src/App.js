0ba8a3d0a5dcba4e6e7b88c4db12b76f3c487b73
James Pesenti <you@example.com>
2022-05-19 14:09:33 -0400

    first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,50 @@
-import logo from './logo.svg';
-import './App.css';
+
+import {BrowserRouter as Router, Routes, Route} from "react-router-dom"
+import {ToastContainer} from "react-toastify"
+import Navbar from "./components/Navbar"
+import PrivateRoute from "./components/PrivateRoute"
+import "react-toastify/dist/ReactToastify.css"
+import CreateListing from "./screens/CreateListing"
+import Explore from "./screens/Explore"
+import Offers from "./screens/Offers"
+import Category from "./screens/Category"
+import Profile from "./screens/Profile"
+import SignIn from "./screens/SignIn"
+import SignUp from "./screens/SignUp"
+import ForgotPassword from "./screens/ForgotPassword"
+import Listing from "./screens/Listing"
+import EditListing from "./screens/EditListing"
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
+  <>
+    <Router>
+      <Routes>
+        <Route path="/" element={<Explore />} />
+        <Route path="/offers" element={<Offers />} />
+        <Route path="/category/:categoryName" element={<Category />} />
+        <Route path="/profile" element={<PrivateRoute />}>
+          <Route path="/profile" element={<Profile />} />
+        </Route>
+        <Route path="/sign-in" element={<SignIn />} />
+        <Route path="/sign-up" element={<SignUp />} />
+        <Route path="/forgot-password" element=
+        {<ForgotPassword />} />
+        <Route path="/create-listing" element=
+        {<CreateListing />} />
+        <Route path="/category/:categoryName/:listingId" element=
+        {<Listing />} />
+        <Route path="/edit-listings/listingId" element={<EditListing/>} />
+      </Routes>
+      <Navbar />
+    </Router>
 
+    <ToastContainer />
+  </>
+  )
+  }
 export default App;
+
+
+
+
